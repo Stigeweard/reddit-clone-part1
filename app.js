a3f160557d3dd577db57594a7c34b94a832ccf36
C S Harding <charliesharding@gmail.com>
2017-01-19 11:19:42 -0600

    pre pull

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
     angular.module('app', [])
         .component('postForm', {
-            controller: function appController() {
+            controller: function () {
                 const vm = this
                 vm.toggle = true;
                 vm.filter = '';
